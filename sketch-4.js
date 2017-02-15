32ae497558afa3f92eae1ae1ea4d576314291b6e
Rita-Lei <ylei@cca.edu>
2017-02-15 08:39:53 -0800

    Update sketch-4.js

--- sketch-4.js
+++ sketch-4.js
@@ -14,7 +14,7 @@ for (var x = 20; x <= width - 20; x = x + 20) {
   for(var y = 20; y <= height - 20; y = y + 20){
     ellipse(x,y,20,20);
     
-     h = h + 0.6;
+     h = h + 1;
   fill(h,100,100)
   }
 }
