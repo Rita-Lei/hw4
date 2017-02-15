823fa28c68c3cccdf5c51d676a385d76d07996c8
Rita-Lei <ylei@cca.edu>
2017-02-15 08:39:15 -0800

    Create sketch-4.js

---
+++ sketch-4.js
@@ -0,0 +1,21 @@
+function setup() { 
+  createCanvas(400, 400);
+} 
+
+function draw() { 
+  
+  background(0);
+  noStroke();
+  
+  colorMode(HSB);
+    var h = 0;
+  
+for (var x = 20; x <= width - 20; x = x + 20) {
+  for(var y = 20; y <= height - 20; y = y + 20){
+    ellipse(x,y,20,20);
+    
+     h = h + 0.6;
+  fill(h,100,100)
+  }
+}
+}
